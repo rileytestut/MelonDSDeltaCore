279aad0a3aa46cd603d7c8909b5a60fc17022dfa
Riley Testut <riley@rileytestut.com>
2021-11-11 12:46:47 -0800

    Changes SwiftPM product type to dynamic library

--- Package.swift
+++ Package.swift
@@ -11,6 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "MelonDSDeltaCore",
+            type: .dynamic,
             targets: ["MelonDSDeltaCore"]),
     ],
     dependencies: [
