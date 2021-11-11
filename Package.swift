8b3f6b29feae22b4002fc0300b78b4f2fae990de
Riley Testut <riley@rileytestut.com>
2021-11-11 12:47:51 -0800

    Updates DeltaCore dependency to use swiftpm branch

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/rileytestut/DeltaCore.git", .branch("main"))
+        .package(url: "https://github.com/rileytestut/DeltaCore.git", .branch("swiftpm"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
