b58cb57bd76d84e652af680818aba7b3d9ba82b6
Riley Testut <riley@rileytestut.com>
2025-07-15 13:53:48 -0500

    Fixes compiling as dynamic framework
    Could not create nested groups in libMelonDS target, so added new libMelonDS group nested in MelonDSDeltaCore that contains additional files.

--- MelonDSDeltaCore/MelonDSDeltaCore.h
+++ MelonDSDeltaCore/MelonDSDeltaCore.h
@@ -17,4 +17,5 @@ FOUNDATION_EXPORT const unsigned char MelonDSDeltaCoreVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <MelonDSDeltaCore/PublicHeader.h>
 #if !STATIC_LIBRARY
 #import <MelonDSDeltaCore/MelonDSEmulatorBridge.h>
+#import <MelonDSDeltaCore/MelonDSTypes.h>
 #endif
